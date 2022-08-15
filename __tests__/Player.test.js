ad811bece82e9aae7a7b9723b22be263c6188b65
Sam Karp <sfcypret@gmail.com>
2022-08-14 22:07:30 -0400

    attack values working, potions working

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -65,3 +65,26 @@ test("subtracts from player's health", () => {
 
     expect(player.health).toBe(0);
 });
+
+test("gets player's attack value", () => {
+    const player = new Player('Dave');
+    player.strength = 10;
+
+    expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
+test('adds a potion to the inventory', () => {
+    const player = new Player('Dave');
+    const oldCount = player.inventory.length
+});
+
+test('uses a potion from inventor', () => {
+    const player = new Player('Dave');
+    player.inventory = [new Potion(), new Potion(), new Potion()];
+    const oldCount = player.inventory.length;
+
+    player.usePotion(1);
+
+    expect(player.inventory.length).toBeLessThan(oldCount);
+});
